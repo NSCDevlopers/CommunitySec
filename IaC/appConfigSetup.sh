41d489dfd31beaf9d7bf94f67c5fd94954ca535b
naman-nihal <naman.nihal02@gmail.com>
2023-02-15 20:37:51 +0530

    Script for IaC

---
+++ IaC/appConfigSetup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "appConfigName: $1";
+echo "Enter Key Vault Name: $2 ";
+# read keyVaultName
+httpHeader='https://'
+suffix='.vault.azure.net'
+
+linkToVault=$httpHeader+$2+$suffix
+
+az appconfig kv set-keyvault -n $1 --key ServerName  --secret-identifier $linkToVault/Secrets/ServerName --yes
+az appconfig kv set-keyvault -n $1 --key DB-Name  --secret-identifier $linkToVault/Secrets/DB-Name --yes
+az appconfig kv set-keyvault -n $1 --key DB-Server-Admin-Password  --secret-identifier $linkToVault/Secrets/DB-Server-Admin-Password --yes
+az appconfig kv set-keyvault -n $1 --key DB-Server-Admin-Login  --secret-identifier $linkToVault/Secrets/DB-Server-Admin-Login --yes
+az appconfig kv set-keyvault -n $1 --key AzureSQLDatabase-ConnectionString  --secret-identifier $linkToVault/Secrets/AzureSQLDatabase-ConnectionString --yes
+az appconfig kv set-keyvault -n $1 --key AppInsights-ConnectionString  --secret-identifier $linkToVault/Secrets/AppInsights-ConnectionString --yes
